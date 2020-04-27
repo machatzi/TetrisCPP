117f8209e3ab104398cf62706d485a795e611e12
machatzi <machatzi@hotmail.com>
2020-04-27 21:36:58 +0300

    Cleaned unused structures and added Vec2 from good pal Chili

--- Engine/Common.h
+++ Engine/Common.h
@@ -6,18 +6,6 @@ enum class MoveDirection {
     DOWN
 };
 
-enum class RectangleType {
-    SOLID = 0,
-    BORDER_ONLY
-};
-
-typedef struct SqrPointsLocation_s {
-    int y_up_side_indx;
-    int y_down_side_indx;
-    int y_center_indx;
-    int x_indx;
-} SqrPointsLocation_t;
-
 typedef struct VectorPosition_s {
     int x;
     int y;
